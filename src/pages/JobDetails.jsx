55329ac606ec2d696510a1cfcc71edf2b65ee5f7
noboKumar <nobokumar227@gmail.com>
2025-05-29 23:51:41 +0600

    add my application

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
 
 const JobDetails = () => {
   const singleJobData = useLoaderData();
-  const { company, company_logo, description } = singleJobData;
+  const { user } = useContext(AuthContext);
+  const { company, company_logo, description, _id } = singleJobData;
+
+  const handleApplyNow = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+
+    const application = {
+      name,
+      description,
+      jobId: _id,
+      applicant: user.email,
+    };
+
+    axios
+      .post("http://localhost:3000/applications", application)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
   return (
     <div>
       <img src={company_logo} alt="logo" />
       <h1 className="text-4xl">{company}</h1>
       <p className="text-xl">{description}</p>
+      <div className="my-10">
+        <form
+          onSubmit={handleApplyNow}
+          className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+        >
+          <legend className="fieldset-legend">Add Your details here</legend>
+
+          <label className="label">Name</label>
+          <input
+            name="name"
+            type="text"
+            className="input"
+            placeholder="Your name"
+          />
+
+          <label className="label">Description</label>
+          <input
+            name="description"
+            type="text"
+            className="textarea"
+            placeholder="Add Your description"
+          />
+          <input className="btn btn-primary" type="submit" value="Apply Now" />
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/MyApplication.jsx
+++ src/pages/MyApplication.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
 
 const MyApplication = () => {
-    return (
-        <div>
-            my application
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const [myApplication, setMyApplication] = useState([]);
+  const email = user?.email;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/applications?email=${email}`)
+      .then((res) => {
+        console.log(res.data);
+        setMyApplication(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [email]);
+  return (
+    <div>
+      my application({myApplication.length})
+      {myApplication.map((application) => (
+        <p key={application._id}>
+          {application.name} {application.description}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default MyApplication;
